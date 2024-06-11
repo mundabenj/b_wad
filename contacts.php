79c024fa64c64bcff51c22b2edffe52d533c832e
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2024-06-11 10:02:52 +0300

    created the database connection

--- contacts.php
+++ contacts.php
@@ -1,12 +1,11 @@
+<?php require_once("includes/db_connect.php"); ?>
 <?php include_once ("templates/heading.php"); ?>
 <?php include_once ("templates/nav.php"); ?>
 <div class="banner">
     <h1>Talk to Us</h1>
 </div>
-
 <div class="row">
     <div class="content">
-
     <h1>Forms</h1>
     <form action="" autocomplete="off" class="contact_us">
         <label for="fullname">Fullname:</label><br>
