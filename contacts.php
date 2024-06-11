88bc3fb9897a23bf31482e55ecf724703bfe9e22
Benjamin Mundama <benjamin.mundama@strathmore.edu>
2024-06-11 13:57:12 +0300

    inserted data into the database

--- contacts.php
+++ contacts.php
@@ -1,29 +1,48 @@
-<?php require_once("includes/db_connect.php"); ?>
-<?php include_once ("templates/heading.php"); ?>
-<?php include_once ("templates/nav.php"); ?>
+<?php 
+    require_once("includes/db_connect.php");
+    include_once ("templates/heading.php");
+    include_once ("templates/nav.php");
+
+    if(isset($_POST["send_message"])){
+        $fullname = mysqli_real_escape_string($conn, addslashes($_POST["fullname"]));
+        $email_address = mysqli_real_escape_string($conn, addslashes($_POST["email_address"]));
+        $subject_line = mysqli_real_escape_string($conn, addslashes($_POST["subject_line"]));
+        $message = mysqli_real_escape_string($conn, addslashes($_POST["message"]));
+
+        $insert_message = "INSERT INTO messages (sender_name, sender_email, subject_line, message) VALUES ('$fullname', '$email_address', '$subject_line', '$message')";
+
+        if ($conn->query($insert_message) === TRUE) {
+            header("Location: contacts.php");
+            exit();
+        } else {
+            echo "Error: " . $insert_message . "<br>" . $conn->error;
+        }
+        $conn->close();
+    }
+?>
 <div class="banner">
     <h1>Talk to Us</h1>
 </div>
 <div class="row">
     <div class="content">
     <h1>Forms</h1>
-    <form action="" autocomplete="off" class="contact_us">
+    <form action="<?php print htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" autocomplete="off" class="contact_us">
         <label for="fullname">Fullname:</label><br>
-        <input type="text" name="fullname" placeholder="Fullname" id="fullname"><br><br>
+        <input type="text" name="fullname" placeholder="Fullname" id="fullname" required ><br><br>
         <label for="email">Email Address:</label><br>
-        <input type="email" name="email_address" id="email" placeholder="Email Address"><br><br>
+        <input type="email" name="email_address" id="email" placeholder="Email Address" required ><br><br>
         <label for="email">Subject:</label><br>
-           <select name="" id="">
+           <select name="subject_line" id="" required >
             <option value="">--Select Subject--</option>
-            <option value="">Email Support</option>
-            <option value="">E-learning Support</option>
-            <option value="">HTML Support</option>
+            <option value="Email Support">Email Support</option>
+            <option value="E-learning Support">E-learning Support</option>
+            <option value="HTML Support">HTML Support</option>
         </select>
         <br><br>
         <label for="message" class="form-label">Message:</label><br>
-        <textarea name="" id="message" rows="10"></textarea>
+        <textarea name="message" id="message" rows="7" required ></textarea>
         <br><br>
-           <input type="submit" value="Send Message">
+           <input type="submit" name="send_message" value="Send Message">
     </form>
 </div>
 <?php include_once("templates/side_bar.php"); ?>
--- sql/db.sql
+++ sql/db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 11, 2024 at 08:44 AM
+-- Generation Time: Jun 11, 2024 at 12:54 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,6 +41,11 @@ CREATE TABLE IF NOT EXISTS `genders` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Truncate table before insert `genders`
+--
+
+TRUNCATE TABLE `genders`;
+--
 -- Dumping data for table `genders`
 --
 
@@ -48,6 +53,36 @@ INSERT INTO `genders` (`genderId`, `gender`, `datecreated`, `dateupdated`) VALUE
 (1, 'Female', '2024-06-11 09:42:10', '2024-06-11 09:42:10'),
 (2, 'Male', '2024-06-11 09:42:10', '2024-06-11 09:42:10'),
 (3, 'Rather not say', '2024-06-11 09:42:10', '2024-06-11 09:42:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
+  `messageId` bigint(10) NOT NULL AUTO_INCREMENT,
+  `sender_name` varchar(50) DEFAULT NULL,
+  `sender_email` varchar(50) DEFAULT NULL,
+  `subject_line` text DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `datecreated` datetime DEFAULT current_timestamp(),
+  `dateupdated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`messageId`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Truncate table before insert `messages`
+--
+
+TRUNCATE TABLE `messages`;
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messageId`, `sender_name`, `sender_email`, `subject_line`, `message`, `datecreated`, `dateupdated`) VALUES
+(1, 'alex', 'alex@yahoo.com', 'E-learning Support', 'Side Bar\r\n\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\r\nIt has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2024-06-11 13:08:51', '2024-06-11 13:08:51');
 
 -- --------------------------------------------------------
 
@@ -65,6 +100,11 @@ CREATE TABLE IF NOT EXISTS `roles` (
   UNIQUE KEY `role` (`role`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Truncate table before insert `roles`
+--
+
+TRUNCATE TABLE `roles`;
 --
 -- Dumping data for table `roles`
 --
@@ -95,7 +135,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-COMMIT;
+
+--
+-- Truncate table before insert `users`
+--
+
+TRUNCATE TABLE `users`;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
